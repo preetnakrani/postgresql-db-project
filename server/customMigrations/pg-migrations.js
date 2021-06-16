7c51ed07cb9c1fa67b150494611b1f5f1e037c4b
Preet Nakrani <titus@dhcp-128-189-128-92.ubcsecure.wireless.ubc.ca>
2021-06-16 11:15:07 -0700

    hosting

--- server/customMigrations/pg-migrations.js
+++ server/customMigrations/pg-migrations.js
@@ -1,12 +1,18 @@
 const config = require(".././config");
 const { Pool } = require("pg");
 
+// const pool = new Pool({
+//   user: process.env.dbuser,
+//   host: process.env.dbhost,
+//   database: process.env.dbname,
+//   password: process.env.dbpassword,
+//   port: process.env.dbport,
+// });
+
 const pool = new Pool({
-  user: process.env.dbuser,
-  host: process.env.dbhost,
-  database: process.env.dbname,
-  password: process.env.dbpassword,
-  port: process.env.dbport,
+  ssl: { rejectUnauthorized: false },
+  connectionString:
+    "postgres://fswsqztzwtxlwf:bb2daf25cc9e3ca0d7e39367f0457274f224e391680704f3c5b09173f5b16102@ec2-34-232-191-133.compute-1.amazonaws.com:5432/de93nratjlk2ca",
 });
 
 // const pool = new Pool({
